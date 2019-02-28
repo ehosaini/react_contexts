bf736729c4fba382e6e47f96c86e553ff344ff79
ehosaini <me.hosaini@gmail.com>
2019-02-28 15:51:50 -0500

    Pass data into the context object

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import UserCreate from './UserCreate'
+import LanguageContext from '../contexts/LanguageContext'
 
 class App extends React.Component {
     constructor(props){
@@ -13,7 +14,10 @@ class App extends React.Component {
             <div className="ui container">
                 <i className="us flag" onClick={() => this.setState({ language: 'english'})}></i>
                 <i className="es flag" onClick={() => this.setState({ language: 'spanish'})}></i>
-                <UserCreate />
+                
+                <LanguageContext.Provider value={this.state.language}>
+                    <UserCreate />
+                </LanguageContext.Provider> 
             </div>
             
         )
